b0cedafe70cf8a9a783ed9a0d68e5d733a4b348e
JERRYR2923 <114004158+JERRYR2923@users.noreply.github.com>
2022-09-21 21:39:10 +0530

    Create pointer.cpp

---
+++ ASSIGNMENT 1/C++/pointer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void update(int *a,int *b) {
+    // Complete this function    
+}
+
+int main() {
+   int a , b;
+   cin >> a ;
+   cin >> b;
+   int add = a + b;
+   int s = a - b ;
+   cout << add << endl;
+   if (s>0){
+       cout << s ;
+   }
+   else {
+       cout << -s ;
+   }
+
+    return 0;
+}
