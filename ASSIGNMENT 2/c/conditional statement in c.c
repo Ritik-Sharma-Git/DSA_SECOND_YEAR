c2a577c600d191376bdd50dbca0fb0a25a20b2b5
JERRYR2923 <114004158+JERRYR2923@users.noreply.github.com>
2022-09-24 23:47:57 +0530

    Create conditional statement in c.c

---
+++ ASSIGNMENT 2/c/conditional statement in c.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+    scanf("%d", &i);
+   
+    if (i==1) {
+    printf("one\n");
+    }
+    else if (i==2) {
+    printf("two\n");
+    }
+
+    else if (i==3) {
+    printf("three\n");
+    }
+    else if (i==4) {
+    printf("four\n");
+    }
+    else if (i==5) {
+    printf("five\n");
+    }
+    else if (i==6) {
+    printf("six\n");
+    }
+    else if (i==7) {
+    printf("seven\n");
+    }
+    else if (i==8) {
+    printf("eight\n");
+    
+    }
+    else if (i==9) {
+    printf("nine\n");
+    }
+    else  {
+    printf("Greater than 9");
+    }
+    return 0;
+    
+}
