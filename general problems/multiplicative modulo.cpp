70d6f38ef5b92087ca5b72f0076ed074f340a4b3
Ritik-Sharma-Git <99575617+Ritik-Sharma-Git@users.noreply.github.com>
2022-10-13 11:00:08 +0530

    Create multiplicative modulo.cpp

---
+++ general problems/multiplicative modulo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    cout<<"enter modulo"<<endl;
+    cin>>k;
+    cout<<"enter element of group"<<endl;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int r=0;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           r=(arr[i]*arr[j])%k;
+            if(r>=k){
+                r=k-r;
+                cout<<r<<" ";
+            }
+            else{
+                cout<<r<<" ";
+            }
+        } cout<<"\n";
+    }
+
+
+    return 0;
+}
