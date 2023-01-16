83ef78190f632c45ae1f3bd2cbfb3a15d5ab9636
Ritik-Sharma-Git <99575617+Ritik-Sharma-Git@users.noreply.github.com>
2023-01-16 10:33:53 +0530

    Create Check palindrome.cpp

---
+++ Assignment 9/Check palindrome.cpp
@@ -0,0 +1,21 @@
+int Solution::solve(string A) {
+    unordered_map<int,int>map;
+    for(int i=0;i<A.size();i++){
+        map[A[i]]++;
+    }
+    int count=0;
+    for(auto i:map){
+        if(i.second%2!=0){
+            count++;
+            if(count>1){
+                return 0;
+            }     
+        }
+    }
+    return 1;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
